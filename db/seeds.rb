cea13bb024cba6276be72e55a41453f40714c061
Shugo Maeda <shugo@ruby-lang.org>
2016-03-09 11:39:34 +0900

    Add a room.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ unless Office.where(name: "松江本社").first
     {office: matsue, name: "小部屋応接"},
     {office: matsue, name: "大部屋"},
     {office: matsue, name: "キッチン"},
+    {office: matsue, name: "小部屋隅テーブル"},
     {office: tokyo, name: "会議室"}
   ])
 else
