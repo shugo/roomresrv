01a6161f5044a891ba02c474231bb3e73ec25f8c
723 <rb.natsumi@gmail.com>
2017-02-20 16:43:00 +0900

    Test add ReservationCancelsController

--- test/controllers/reservation_cancels_controller_test.rb
+++ test/controllers/reservation_cancels_controller_test.rb
@@ -4,4 +4,13 @@ class ReservationCancelsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @reservationcancel = reservations(:mendan)
+  end 
+
+
+  test "should reservation cancels" do
+    post "/reservations/#{@reservationcancel.id}/reservation_cancels" ,params:{reservation_cancel: {id: @reservationcancel.id,start_on:@reservationcancel.start_at}}
+    assert_response :redirect
+  end
 end
