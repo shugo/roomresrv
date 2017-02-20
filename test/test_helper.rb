980cadc6cc4ca2af7fb5ca7a5a1b9271b10eac15
Shugo Maeda <shugo@ruby-lang.org>
2017-02-20 10:45:30 +0900

    Upload coverage reports to codecov.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,23 @@
 require "simplecov"
 SimpleCov.start("rails")
+if ENV["UPLOAD_TO_CODECOV"]
+  require "codecov"
+  module IgnoreFormatError
+    def format(*args)
+      super(*args)
+    rescue => e
+      { 
+        "error" => {
+          "message" => e.message,
+          "class" => e.class.name,
+          "backtrace" => e.backtrace
+        }
+      }
+    end
+  end
+  SimpleCov::Formatter::Codecov.send(:prepend, IgnoreFormatError)
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
