b506dd8b856273ab9c85cebaecb0f968d6067136
723 <rb.natsumi@gmail.com>
2017-02-21 13:31:01 +0900

    ReservationCancel add test

--- test/controllers/reservation_cancels_controller_test.rb
+++ test/controllers/reservation_cancels_controller_test.rb
@@ -6,11 +6,15 @@ class ReservationCancelsControllerTest < ActionDispatch::IntegrationTest
   # end
   setup do
     @reservation = reservations(:kaigi)
+    @reservation.start_at += 7.day
   end 
 
 
   test "should reservation cancels" do
-    post reservation_reservation_cancels_path(@reservation) ,params:{reservation_cancel:  {start_on: @reservation.start_at + 7.day}}
+    post reservation_reservation_cancels_path(@reservation) ,params:{reservation_cancel:  {start_on: @reservation.start_at}}
     assert_redirected_to(controller:"calendar",action:"index")
+    r = ReservationCancel.find_by(start_on: @reservation.start_at)
+    assert_equal(false,r.nil?)
   end
+
 end
