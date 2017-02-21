58176dd66c5af262c6ef06a5a2f5a6175ed29d65
723 <rb.natsumi@gmail.com>
2017-02-21 13:51:36 +0900

    Confirm model creation

--- test/controllers/reservation_cancels_controller_test.rb
+++ test/controllers/reservation_cancels_controller_test.rb
@@ -13,8 +13,7 @@ class ReservationCancelsControllerTest < ActionDispatch::IntegrationTest
   test "should reservation cancels" do
     post reservation_reservation_cancels_path(@reservation) ,params:{reservation_cancel:  {start_on: @reservation.start_at}}
     assert_redirected_to(controller:"calendar",action:"index")
-    r = ReservationCancel.find_by(start_on: @reservation.start_at)
-    assert_equal(false,r.nil?)
+    assert(@reservation.reservation_cancels(true))
   end
 
 end
